a19b02bf47653ac1adbf75ef93402d80c6e43d72
Tanver Ahmed Likhon <39611778+TanverLikhon@users.noreply.github.com>
2021-07-25 15:24:04 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const form = document.getElementById('form');
+
+form.addEventListener('submit',(e)>= {
+  
+    window.alert("ddd");
+    console.log("entered");
+    document.getElementById("result").innerHTML = "Hello";
+});
